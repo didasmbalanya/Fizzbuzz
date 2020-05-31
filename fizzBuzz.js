afbf1da10e1e81d0f5af3f573c2071f692b91a8b
Didas Mbalanya <didasmbalanya@gmail.com>
2020-05-31 22:02:31 +0200

    final commit v1

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,15 @@
-for (let i = 0; i <= 100; i++) {
-  if (i % 15 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
+const fizzBuzz = (n) => {
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
   }
-}
+};
 
-console.log(72 % 3);
+fizzBuzz(100);
